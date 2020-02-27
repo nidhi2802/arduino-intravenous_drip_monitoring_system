932dacb053e902e77297dfb2d25059cc6918885c
nidhi2802 <61545104+nidhi2802@users.noreply.github.com>
2020-02-27 17:18:24 +0530

    Add files via upload

---
+++ GSM.ino
@@ -0,0 +1,34 @@
+
+ #include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+
+void setup()
+{
+  mySerial.begin(9600);    
+  Serial.begin(9600);    
+  delay(100);
+}
+
+
+void loop()
+{
+  if (Serial.available()>0)
+   switch(Serial.read())
+  {
+    case 's':
+      mySerial.println("AT+CMGF=1");   
+     delay(1000); 
+     mySerial.println("AT+CMGS=\"+916351584610\"\r");
+     delay(1000);
+     mySerial.println("Hello");
+     delay(100);
+     mySerial.println((char)26); 
+      delay(1000);
+      break;
+      
+     }
+
+ if (mySerial.available()>0)
+   Serial.write(mySerial.read());
+}
